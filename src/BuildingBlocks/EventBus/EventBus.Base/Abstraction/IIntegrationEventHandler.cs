f51c859d99179851f323b1d09a7c2ad409f65154
Cafer AYDIN <zng.caferaydin@gmail.com>
2024-11-18 16:09:58 +0300

    Add project files.

---
+++ src/BuildingBlocks/EventBus/EventBus.Base/Abstraction/IIntegrationEventHandler.cs
@@ -0,0 +1,14 @@
+﻿using EventBus.Base.Events;
+
+namespace EventBus.Base.Abstraction
+{
+    public interface IIntegrationEventHandler<T_IntegrationEvent> : IntegrationEventHandler where T_IntegrationEvent : IntegrationEvent
+    {
+        Task Handle(T_IntegrationEvent @event);
+    }
+
+    public interface IntegrationEventHandler
+    {
+
+    }
+}
